400c8fe1878a8cdbebec28195932138ee5cdb770
Alexander Shpilka <alexander.shpilka@kruschecompany.com>
2015-02-22 09:58:39 +0200

    connect to meteor.com

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -1,8 +1,52 @@
-var app = new Asteroid("localhost:3000");
+var application = {
+    init: function () {
+        application.secondCount = 4;
+        application.app = new Asteroid("http://iliketasty.meteor.com");
+        application.app.subscribe("addedTimer");
+        var timer = application.app.getCollection("tasks");
+        var laundryTimerRQ = timer.reactiveQuery({description: "Do the laundry"});
+        // Log the array of results
+        console.log(laundryTimerRQ.result);
+        // Listen for changes
+        laundryTimerRQ.on("change", function () {
+            console.log(laundryTimerRQ.result);
+        });
+
+//        application.startTimer(application.secondCount);
+    },
+    startTimer: function (secondCount) {
+        var interval = window.setInterval(function () {
+            secondCount--;
+            chrome.browserAction.setBadgeText({text: '' + secondCount})
+            if (secondCount === 0) {
+                application.endTimer();
+                window.clearInterval(interval);
+            }
+        }, 1000);
+    },
+    endTimer: function () {
+        if (Notification) {
+            var notification = new Notification('Поспешите на кухню!', {
+                icon: 'http://yellowicons.com/wp-content/uploads/Food-Icon-1.png',
+                body: "Эй! Очередной шаг приготовления вашего блюда подошел к концу. Поспешите на кухню и продолжайте готовить!",
+            });
+            audioNotification();
+
+            function audioNotification() {
+                var yourSound = new Audio('../../resourses/sounds-924-long-chime-sound.mp3');
+                yourSound.play();
+            }
+        }
+    }
+}
+application.init();
+
+
+var app = new Asteroid("iliketasty.meteor.com");
 
 init();
 
-function init(){
+function init() {
     if (window.localStorage.getItem('isLogged') == "true") {
         $('#logged').css('display', 'block');
         $('#login').css('display', 'none');
